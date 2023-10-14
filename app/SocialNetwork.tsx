4a6b4a66d7b08f038fe0818c930ab7423ab495b0
Alexandre Mogère <heristop@yahoo.fr>
2023-10-14 18:35:25 +0200

    [Project] Initial commit 🚀

---
+++ app/SocialNetwork.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from 'next/image';
+import { SocialNetworkProps } from './types';
+
+const SocialNetwork: React.FC<SocialNetworkProps> = ({
+    href,
+    iconSrc,
+    title,
+    description,
+    delay = 0
+}) => {
+    return (
+        <div
+            className={`flex items-start justify-center p-2 w-full md:w-1/4 rounded hover:bg-gray-200 hover:opacity-90 transition duration-300 ease-in-out fade-in-up-bounce`}
+            style={{ animationDelay: `${delay}ms` }}
+        >
+            <a href={href} target="blank">
+                <Image src={iconSrc} alt={description} width={300} height={100} className="object-cover w-[300px] h-[100px] rounded-lg overflow-hidden shadow-lg" />
+
+                <div className="data p-4">
+                    <h2 className="text-xl font-bold mb-2">{title}</h2>
+                    <p className="text-gray-600 text-sm">{description}</p>
+                </div>
+            </a>
+        </div>
+    );
+}
+
+export default SocialNetwork;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      }
+    }
   },
   plugins: [],
 }
-export default config
+
+export default config;
