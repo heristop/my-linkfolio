2d002519b303764e13458492c7b2912e6141b692
Alexandre Mogère <heristop@yahoo.fr>
2023-10-23 23:11:10 +0200

    [Project] Intgrated LinkFolio package

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import LinkFolio from "./components/LinkFolio";
-import UserProfile from "./components/UserProfile";
-import SocialLinks from "./components/SocialLinks";
-
-export { LinkFolio, UserProfile, SocialLinks };
